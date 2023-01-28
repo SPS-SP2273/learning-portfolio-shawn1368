861af2d073b256a140e60ef47aa4195a7b4ee6d2
shawn1368 <huaen.lim@gmail.com>
2023-01-28 22:28:45 +0800

    Pandas (Good), Pandas (Nice)

--- others/matplotlib_testing.py
+++ others/matplotlib_testing.py
@@ -36,11 +36,14 @@ fig1, ax1 = plt.subplots(subplot_kw={"projection": "3d"})
 surf = ax1.plot_surface(x_1, y_1, z_1, cmap="coolwarm")
 # ax1.plot_wireframe(x_1, y_1, z_1)
 fig1.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
 # %%
 # Imshow
 Z = np.random.uniform(0, 1, (16, 16))
 plt.imshow(Z, cmap="viridis")
 plt.colorbar()
+plt.show()
 
 # %%
 # 3D
@@ -51,14 +54,16 @@ Z = X ** 2 - 2 * Y * X + Y ** 3
 
 fig, ax = plt.subplots()
 ax.contourf(X, Y, Z)
-ax.colorbar()
-
+# ax.colorbar()
+plt.show()
 # %%
 # Scatter
 scatter_data_x = np.random.randint(0, 101, 100)
 scatter_data_y = np.random.randint(0, 101, 100)
 fig2, ax2 = plt.subplots()
 ax2.scatter(scatter_data_x, scatter_data_y)
+
+plt.show()
 # %%
 # Bar
 x_axis = ["Thing 1", "Thing 2", "Thing 3", "Thing 4"]
@@ -66,3 +71,4 @@ y_axis = np.array([275, 28, 182, 375])
 
 fig3, ax3 = plt.subplots()
 ax3.barh(x_axis, y_axis)
+fig3.show()
